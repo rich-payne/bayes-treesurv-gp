ff58ab576356282128d6f7b7fa5a989599953a07
Richard Payne <paynedrichard@gmail.com>
2018-01-25 13:36:24 -0600

    Fixes gradient and Hessian

--- src/get_grad.m
+++ src/get_grad.m
@@ -1,5 +1,4 @@
 function out = get_grad(f,a,b,Astar,Sigmainv,ns)
-    val = -.5 * Sigmainv * f - .5 * diag(Sigmainv) .* f + ...
-        1/Astar .* sum(Sigmainv,2) .* sum(Sigmainv * f) ;   
-    out = ns - exp(f) .* (a + b) + val;
+    val = 1/Astar .* sum(Sigmainv,2) .* sum(Sigmainv * f) ;   
+    out = ns - exp(f) .* (a + b) - Sigmainv * f + val;
 end
--- src/get_hess.m
+++ src/get_hess.m
@@ -1,5 +1,5 @@
 function out = get_hess(f,a,b,Astar,Sigmainv)
-    tmp = -.5*Sigmainv;
-    out = diag(-exp(f) .* (a + b) - diag(Sigmainv),0) + tmp - diag(diag(tmp)) + ...
+    %tmp = -.5*Sigmainv;
+    out = diag(-exp(f) .* (a + b)) - Sigmainv + ...
         + 1./Astar .* (sum(Sigmainv,2) * sum(Sigmainv,1));
 end
