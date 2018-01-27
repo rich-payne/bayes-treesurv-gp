e78dc8142bce296daea6070b486bf17b91188adc
Richard Payne <paynedrichard@gmail.com>
2018-01-27 11:26:12 -0600

    updates Sigma for K=1

--- src/get_sigma.m
+++ src/get_sigma.m
@@ -1,5 +1,9 @@
 function Sigma = get_sigma(Z,tau,l,nugget)
     K = length(Z);
-    Sigma = tau^2 * exp(-(1/(2*l^2)) * squareform(pdist(Z,'squaredeuclidean')) ) + diag(ones(1,K)*(nugget.^2));
+    if K > 1
+        Sigma = tau^2 * exp(-(1/(2*l^2)) * squareform(pdist(Z,'squaredeuclidean')) ) + diag(ones(1,K)*(nugget.^2));
+    else
+        Sigma = tau^2 + nugget^2;
+    end
 end
     
