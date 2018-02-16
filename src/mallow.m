475c63ed92721228edd44e95df572816bca35c20
Richard Payne <paynedrichard@gmail.com>
2018-02-16 11:08:26 -0600

    more general mallow function

--- src/mallow.m
+++ src/mallow.m
@@ -1,18 +1,38 @@
-function [trees,LLIKES] = mallow(output)    
+function [trees,LLIKES,whichout] = mallow(varargin) 
+    % submit output files for a mallows plot
+    maxtree = 0;
+    nout = length(varargin);
+    for ii=1:nout
+        output = varargin{ii};
+        maxtree = max(max(output.treesize),maxtree);
+    end
+        
+        
     % Mallow's C_p type plot
-    maxtree = max(output.treesize);
+    % maxtree = max(output.treesize);
     LLIKES = [];
     trees = cell(maxtree,1);
+    whichout = [];
     for ii=1:maxtree
-        ind = output.treesize == ii;
-        llikes = output.llike(ind);
-        TREES = output.Trees(ind);
-        [tmpll,I] = max(llikes);
-        if ~isempty(tmpll)
-            LLIKES(ii) = tmpll;
-            trees{ii} = TREES{I};
-        else
-            LLIKES(ii) = NaN;
+        tmpll_old = -Inf;
+        LLIKES(ii) = NaN;
+        for jj=1:nout
+            output = varargin{jj};
+            ind = output.treesize == ii;
+            llikes = output.llike(ind);
+            TREES = output.Trees(ind);
+            [tmpll,I] = max(llikes);
+            if ~isempty(tmpll)
+                if tmpll > tmpll_old
+                    LLIKES(ii) = tmpll;
+                    trees{ii} = TREES{I};
+                    whichout(ii) = jj;
+                    tmpll_old = tmpll;
+                end
+            end
+%             else
+%                 LLIKES(ii) = NaN;
+%             end
         end
     end
     plot(1:maxtree,LLIKES,'-o')
