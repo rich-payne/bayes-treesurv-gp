137bcf80f641ea862ab76273165554a3a0903d5c
Richard Payne <paynedrichard@gmail.com>
2018-04-26 14:25:03 -0500

    Adds documentation to get_f

--- src/get_f.m
+++ src/get_f.m
@@ -1,5 +1,40 @@
+%    bayes-treesurv-gp provides a Bayesian tree partition model to flexibly 
+%    estimate survival functions in various regions of the covariate space.
+%    Copyright (C) 2017-2018  Richard D. Payne
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%    This function obtains the MAP estimate of the latent Gaussian process
+%    used to model the hazad function.
+%
+%    INPUTS
+%    ns: a vector containing the number of uncensored observations which
+%      fall into each bin.
+%    a: a vector containing the sum of all of the distances between the
+%      survival times and the lower limit of their respective bins.
+%    b: a vector containing the widths of each bin multiplied by the number
+%      of observations which survive past the upper limit of the bin.
+%    Z: a vector containing the centers of the bins
+%    tau: the magnitude hyperparameter for the covariance function of the
+%      latent process, f.
+%    l: the length-scale hyperparameter for the covariance function of the
+%      latent process, f. 
+%    nugget: unused argument.  Ignore.
+%    eps: the threshold used to assess convergence in Newton's method.
+
 function [f_final,marg_y,Omegainv] =  get_f(ns,a,b,Z,tau,l,nugget,eps)
-    %warning('off','MATLAB:nearlySingularMatrix')
+    % warning('off','MATLAB:nearlySingularMatrix')
     if(size(Z,1) < size(Z,2))
         error('Z must be a column vector');
     end
