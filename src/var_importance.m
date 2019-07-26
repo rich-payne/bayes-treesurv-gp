c202a838b292af5f00b100db594407684bc9730c
Richard Payne <richard.payne@lilly.com>
2019-07-26 13:43:44 -0400

    adds type to var_importance

--- src/var_importance.m
+++ src/var_importance.m
@@ -1,7 +1,13 @@
-function V = var_importance(output, X)
-   a = max(output.llike);
-   log_denom = a + log(sum(exp(output.llike - a)));
-   w = exp(output.llike - log_denom);
+function V = var_importance(output, X, type)
+   if strcmp(type, 'like')
+       a = max(output.llike);
+       log_denom = a + log(sum(exp(output.llike - a)));
+       w = exp(output.llike - log_denom);
+   elseif strcmp(type, 'post')
+       a = max(output.llike + output.lprior);
+       log_denom = a + log(sum(exp(output.llike + output.lprior - a)));
+       w = exp(output.llike + output.lprior - log_denom);
+   end
    w_v = zeros(size(X, 2), 1);
    for ii = 1:length(output.Trees)
      w_v = w_v + w(ii) * var_count(output.Trees{ii}, X);
